8556f29e8a8106c288ff4e76d77f1b149803df3b
Yuvrajs-006 <yuvrajshr9@gmail.com>
2025-01-27 22:28:02 +0530

    Solution to Simple Calculator

---
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+
+
+int main() {
+    int a,b;
+    scanf("%d%d");
+    char n;
+    scanf("%c",&n);
+    char o[4]="+-/*"
+    if(n==o[0]){
+        printf("YEs");
+    }
+    return 0;
+}
