428a62d82ef10c061d07d81577abf8496ad86005
Yuvraj Sharma <yuvraj0242.becse24@chitkara.edu.in>
2025-03-15 16:51:10 +0530

    Solution to Find the Frequency of Elements in an Array

--- Find the Frequency of Elements in an Array@@676c02002dbc8c3e7a280b73/code.c
+++ Find the Frequency of Elements in an Array@@676c02002dbc8c3e7a280b73/code.c
@@ -9,46 +9,15 @@ int main(){
 	for(int i=0;i<s;i++){
 		scanf("%d",&arr[i]);
 	}
-	for(int i=0;i<s;i++){
-		for(int j=0;j<s-i;j++){
-			if(arr[j]>arr[j+1]){
-				int temp =arr[j];
-				arr[j]=arr[j+1];
-				arr[j+1]=temp;
-			}
-		}
-	}
-	int arr1[s];
-	int c=0;
-	if(s%2==0){
-		for(int i=0;i<s;i++){
-			if(arr[i]!=arr[i+1]){
-				arr1[c]=arr[i];
-				c++;
-			}
-		}
-	}
-	else{
-		for(int i=0;i<s;i++){
-			if(i==s-1){
-				arr1[c]=arr[i];
-				c++;
-			}
-			if(arr[i]==arr[i+1]){
-				arr1[c]=arr[i];
-				c++;
-			}
-		}
-	}
-	for(int i=0;i<c;i++){
-		int num=arr1[i];
-		int count =0;
-		for(int j=0;j<s;j++){
-			if(arr[j]==num){
-				count++;
-			}
-		}
-		printf("%d %d\n",num,count);
-		count =0;
-	}
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n-i;j++){
+            if(arr[j]>arr[j+1]){
+                int temp =arr[j+1];
+                arr[j+1]=arr[j];
+                arr[j]=temp;
+            }
+        }
+    }
+    int arr1[]=arr[];
+
 }
