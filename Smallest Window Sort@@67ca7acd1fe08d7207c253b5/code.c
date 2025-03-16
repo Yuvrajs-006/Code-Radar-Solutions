478858e376dc732c4bb33f23523527ff9a4c6c1d
Yuvraj Sharma <yuvraj0242.becse24@chitkara.edu.in>
2025-03-16 22:15:20 +0530

    Solution to Smallest Window Sort

--- Smallest Window Sort@@67ca7acd1fe08d7207c253b5/code.c
+++ Smallest Window Sort@@67ca7acd1fe08d7207c253b5/code.c
@@ -1,26 +1,33 @@
-int findUnsortedSubarray(int arr[],int n){
-    int l=0;
-    int r=0;
-    for(int i=0;i<n-1;i++){
-        if(arr[i]>arr[i+1]){
-            l=i;
+#include <stdio.h>
+#include <stdlib.h>
+
+void smallestWindowSort(int arr[], int n) {
+    int l = -1, r = -1;
+
+    // Find the first unsorted index from the left
+    for (int i = 0; i < n - 1; i++) {
+        if (arr[i] > arr[i + 1]) {
+            l = i;
             break;
         }
-    }
-    for(int i=n-1;i>=0;i--){
-        if(arr[i]<arr[i-1]){
-            r=i;
-            break;
-        }
-    }
-    if(r-l==0){
-        return 0;
-    }
-    else if(arr[0]>arr[n-1]){
-        return n;
-    }
-    else{
-        return (r-l)+1;
     }
 
+    // If the array is already sorted, return
+    if (l == -1) return;
+
+    // Find the first unsorted index from the right
+    for (int i = n - 1; i > 0; i--) {
+        if (arr[i] < arr[i - 1]) {
+            r = i;
+            break;
+        }
+    }
+
+    // Sort only the subarray from l to r
+    qsort(arr + l, r - l + 1, sizeof(int), (int (*)(const void *, const void *))cmp);
+}
+
+// Comparison function for qsort
+int cmp(const void *a, const void *b) {
+    return (*(int *)a - *(int *)b);
 }
